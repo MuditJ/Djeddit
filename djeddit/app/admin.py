c716642417be05fca4d203b3d30195bc1adbc49e
Mudit <muditj06@gmail.com>
2019-11-14 19:24:54 +0530

    Hopefully fixed stupidschema errors

--- djeddit/app/admin.py
+++ djeddit/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserProfile,Sub,Post,Comment
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Sub)
+admin.site.register(Post)
+admin.site.register(Comment)
--- djeddit/app/models.py
+++ djeddit/app/models.py
@@ -3,37 +3,60 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+DEFAULT_USER = User.objects.first() #Mudit-Admin
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
     bio = models.CharField(max_length = 200,blank = True)
-    username = models.CharField(max_length = 20, blank = False)
-    email = models.EmailField()
+    #These fields are present in the User model
+    #username = models.CharField(max_length = 20, blank = False)
+    #email = models.EmailField()
+    #password = models.CharField(max_length = 20)    
     karma = models.IntegerField(default = 0)
-    password = models.CharField(max_length = 20)
     created_on = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
 
 class Sub(models.Model):
     name = models.CharField(max_length = 30)
+    created_by = models.ForeignKey(User,on_delete = models.CASCADE, null = True)
+    description = models.CharField(max_length = 100, default = 'A Djeddit sub')
     created_on = models.DateField(auto_now_add = True)
-    posts = models.ForeignKey(Post,on_delete = models.CASCADE)
-    members = models.ManyToManyField(UserProfile)
-         
+    #FIX THIS
+    members = models.ManyToManyField(UserProfile,blank = False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def save(self,*args,**kwargs):
+        #self.members = self.created_by
+        print(f'Members of this sub are: {self.created_by}')
+        super().save(*args,**kwargs)
+
 class Post(models.Model):
     title = models.TextField(max_length = 50)
     content = models.TextField()
-    created_by = models.OneToOneField(UserProfile,on_delete = models.CASCADE)
-    upvotes = models.IntegerField()
-    downvotes = models.IntegerField()
-    sub_posted_on = models.OneToOneField(Sub)
+    created_by = models.ForeignKey(UserProfile,on_delete = models.CASCADE,null = False)
+    upvotes = models.IntegerField(blank = True,default = 0)
+    downvotes = models.IntegerField(blank = True,default = 0)
+    sub_posted_on = models.ForeignKey('Sub', on_delete = models.CASCADE)
     created_on = models.DateField(auto_now_add = True)
 
+    def __str__(self):
+        return f'{(self.title)} -> {self.sub_posted_on}'
+
 class Comment(models.Model):
-    parent_post = models.ForeignKey(Post, null = False)
-    created_by = models.OneToOneField(UserProfile,on_delete = models.CASCADE)
-    upvotes = models.IntegerField()
-    downvotes = models.IntegerField()
+    parent_post = models.ForeignKey(Post, on_delete = models.CASCADE, null = False)
+    created_by = models.ForeignKey(UserProfile,on_delete = models.CASCADE)
+    content = models.TextField(default = 'Mitigating schema mistake')
+    upvotes = models.IntegerField(blank = True, default = 0)
+    downvotes = models.IntegerField(blank = True, default = 0)
     created_on = models.DateTimeField(auto_now_add = True)
 
+    def __str__(self):
+        return f'{self.content[:20]}'
+        
 class Vote(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE, null = True)
     comment = models.ForeignKey(Comment,  on_delete = models.CASCADE, null = True)
